3d03230be8f022d6f819f5c807741a867ad3c69f
Onur Yörük <onuryoruk@me.com>
2022-01-26 10:11:55 +0700

    Add EssentialFeedIntegrationTests target to separate the potentially slower cache integration tests from the fast unit/isolated tests

---
+++ EssentialFeedCacheIntegrationTests/EssentialFeedCacheIntegrationTests.swift
@@ -0,0 +1,14 @@
+//
+//  EssentialFeedCacheIntegrationTests.swift
+//  EssentialFeedCacheIntegrationTests
+//
+//  Created by Onur Yörük on 26.01.22.
+//
+
+import XCTest
+
+class EssentialFeedCacheIntegrationTests: XCTestCase {
+
+    
+
+}
--- EssentialFeedTests/Feed Cache/CodableFeedStoreTests.swift
+++ EssentialFeedTests/Feed Cache/CodableFeedStoreTests.swift
@@ -38,7 +38,7 @@ class CodableFeedStoreTests: XCTestCase, FailableFeedStore {
     func test_retrieve_deliversFoundValuesOnNonEmptyCache () {
         let sut = makeSUT()
         let feed = uniqueImageFeed().local
-        let timestamp = Date()
+        let timestamp = Date() 
 
         insert((feed, timestamp), to: sut)
         expect(sut, toRetrieve: .found(feed: feed, timeStamp: timestamp))
