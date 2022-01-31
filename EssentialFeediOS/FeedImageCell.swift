f5a6390dd0f58760db88a9ad3b106be275f3f7d1
Onur Yörük <onuryoruk@me.com>
2022-01-31 10:15:50 +0700

    Render loaded feed on successfully load completion

---
+++ EssentialFeediOS/FeedImageCell.swift
@@ -0,0 +1,18 @@
+//
+//  FeedImageCell.swift
+//  EssentialFeediOS
+//
+//  Created by Onur Yörük on 31.01.22.
+//
+
+import Foundation
+import UIKit
+
+public final class FeedImageCell: UITableViewCell {
+
+    public let locationContainer = UIView()
+    public let locationLabel = UILabel()
+    public let descriptionLabel = UILabel()
+
+}
+
