ad549108a981cfc11ae05c887e08d5c56592cb48
Onur Yörük <onuryoruk@me.com>
2022-01-31 11:26:55 +0700

    Feed image view loading is visible while loading image

--- EssentialFeediOS/FeedImageCell.swift
+++ EssentialFeediOS/FeedImageCell.swift
@@ -13,6 +13,7 @@ public final class FeedImageCell: UITableViewCell {
     public let locationContainer = UIView()
     public let locationLabel = UILabel()
     public let descriptionLabel = UILabel()
+    public let feedImageContainer = UIView()
 
 }
 
